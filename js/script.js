6b7b28ccd1c18339b2de31b40a1a5e02299c7bd9
naiara-ada <naiara.ada@gmail.com>
2023-11-08 11:37:47 +0100

    increase elements

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+const lista = document.getElementById('lista');
+const boton = document.getElementById('agregar');
+
+
+boton.addEventListener('click', function (){
+    let elemento = prompt('Introduce un elemento');
+    let hija = document.createElement('li');
+    hija.textContent = elemento;
+    lista.appendChild(hija);
+    console.log(lista);
+})
